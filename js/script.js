007aa70c00b71f98d1c9146008ea600a151b2771
Ammar Arab <132087789+0ammar@users.noreply.github.com>
2023-06-20 04:09:38 +0300

    N

--- js/script.js
+++ js/script.js
@@ -1,35 +1,28 @@
 let navbar = document.querySelector('.header .flex .navbar');
-
 document.querySelector('#menu-btn').onclick = () =>{
    navbar.classList.toggle('active');
 }
 
 let account = document.querySelector('.user-account');
-
 document.querySelector('#user-btn').onclick = () =>{
    account.classList.add('active');
 }
-
 document.querySelector('#close-account').onclick = () =>{
    account.classList.remove('active');
 }
 
 let myOrders = document.querySelector('.my-orders');
-
 document.querySelector('#order-btn').onclick = () =>{
    myOrders.classList.add('active');
 }
-
 document.querySelector('#close-orders').onclick = () =>{
    myOrders.classList.remove('active');
 }
 
 let cart = document.querySelector('.shopping-cart');
-
 document.querySelector('#cart-btn').onclick = () =>{
    cart.classList.add('active');
 }
-
 document.querySelector('#close-cart').onclick = () =>{
    cart.classList.remove('active');
 }
@@ -42,13 +35,11 @@ window.onscroll = () =>{
 
 let slides = document.querySelectorAll('.home-bg .home .slide-container .slide');
 let index = 0;
-
 function next(){
    slides[index].classList.remove('active');
    index = (index + 1) % slides.length;
    slides[index].classList.add('active');
 }
-
 function prev(){
    slides[index].classList.remove('active');
    index = (index - 1 + slides.length) % slides.length;
@@ -56,7 +47,6 @@ function prev(){
 }
 
 let accordion = document.querySelectorAll('.faq .accordion-container .accordion');
-
 accordion.forEach(acco =>{
    acco.onclick = () =>{
       accordion.forEach(remove => remove.classList.remove('active'));
